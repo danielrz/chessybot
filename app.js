86f9b4ebbe042580e91b9e6b4c02bdc9248e5f30
danielr <danielr@liveperson.com>
2016-10-12 13:50:26 +0300

    next move fen

---
+++ app.js
@@ -0,0 +1,183 @@
+/**
+ * Created by danielr on 10/12/16.
+ */
+var builder = require('botbuilder');
+var restify = require('restify');
+var engine = require('./engine/engine');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+
+//=========================================================
+// Activity Events  (skype only)
+//=========================================================
+
+bot.on('conversationUpdate', function (message) {
+    // Check for group conversations
+    if (message.address.conversation.isGroup) {
+        // Send a hello message when bot is added
+        if (message.membersAdded) {
+            message.membersAdded.forEach(function (identity) {
+                if (identity.id === message.address.bot.id) {
+                    var reply = new builder.Message()
+                        .address(message.address)
+                        .text("Hello everyone! I'm your chess buddy ;)");
+                    bot.send(reply);
+                }
+            });
+        }
+
+        // Send a goodbye message when bot is removed
+        if (message.membersRemoved) {
+            message.membersRemoved.forEach(function (identity) {
+                if (identity.id === message.address.bot.id) {
+                    var reply = new builder.Message()
+                        .address(message.address)
+                        .text("Goodbye");
+                    bot.send(reply);
+                }
+            });
+        }
+    }
+});
+
+bot.on('contactRelationUpdate', function (message) {
+    if (message.action === 'add') {
+        var name = message.user ? message.user.name : null;
+        var reply = new builder.Message()
+            .address(message.address)
+            .text("Hello %s... Thanks for adding me. I'm chessyBot, your chess buddy! Say 'hi' to see some great demos.", name || 'there');
+        bot.send(reply);
+    } else {
+        // delete their data
+    }
+});
+
+bot.on('deleteUserData', function (message) {
+    // User asked to delete their data
+});
+
+//=========================================================
+// Bots Middleware
+//=========================================================
+
+// Anytime the major version is incremented any existing conversations will be restarted.
+bot.use(builder.Middleware.dialogVersion({ version: 1.0, resetCommand: /^reset/i }));
+
+//=========================================================
+// Bots Global Actions
+//=========================================================
+
+bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^goodbye/i });
+bot.beginDialogAction('help', '/help', { matches: /^help/i });
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', [
+    function (session) {
+        // Send a greeting and show help.
+        var card = new builder.HeroCard(session)
+            .title("ChessyBot - your chess buddy")
+            .text("Let me help you with your next move!")
+            .images([
+                builder.CardImage.create(session, "https://placeholdit.imgix.net/~text?txtsize=35&txt=ChessyBot&w=300&h=160")
+            ]);
+        var msg = new builder.Message(session).attachments([card]);
+        session.send(msg);
+        session.send("Hi... I'm ChessyBot. Let me help you to make the next winning move!");
+        session.beginDialog('/help');
+    },
+    function (session, results) {
+        // Display menu
+        session.beginDialog('/menu');
+    },
+    function (session, results) {
+        // Always say goodbye
+        session.send("Ok... See you later!");
+    }
+]);
+
+bot.dialog('/help', [
+    function (session) {
+        session.endDialog("Global commands that are available anytime:\n\n* menu - Exits a demo and returns to the menu.\n* goodbye - End this conversation.\n* help - Displays these commands.");
+    }
+]);
+
+bot.dialog('/menu', [
+    function (session) {
+        builder.Prompts.choice(session, "How would you like me to predict your next move?", "fen|moves|(quit)");
+    },
+    function (session, results) {
+        if (results.response && results.response.entity != '(quit)') {
+            // Launch engine dialog
+            session.beginDialog('/' + results.response.entity);
+        } else {
+            // Exit the menu
+            session.endDialog();
+        }
+    },
+    function (session, results) {
+        // The menu runs a loop until the user chooses to (quit).
+        session.replaceDialog('/menu');
+    }
+]).reloadAction('reloadMenu', null, { matches: /^menu|show menu/i });
+
+bot.dialog('/fen', [
+    function (session) {
+        //session.send("please enter the FEN text. Leave empty for the initial chess game. Example: rnbqkb1r/pp3ppp/4pn2/3p4/2PP4/2N5/PP3PPP/R1BQKBNR w KQkq -");
+        builder.Prompts.text(session, "please enter the FEN of your current board. Leave empty for the initial chess game. Example: rnbqkb1r/pp3ppp/4pn2/3p4/2PP4/2N5/PP3PPP/R1BQKBNR w KQkq -");
+    },
+    function (session, results) {
+        ///TODO: perform FEM validation step here
+        ///TODO: use function
+        session.send("ok. let me check the next best move for you....");
+        engine
+            .nextMove(results.response)
+            .then((result) => {
+                session.send("I suggest you that one: '%s'", result.nextMove);
+
+                ///TODO: call a dialog which prompts for help on visualizing on the board the next move and redirects to lichess or a are you curious to know what is your opponent next best move
+
+                // End
+                session.endDialog();
+            });
+    }
+
+]);
+
+bot.dialog('/moves', [
+    function (session) {
+        //session.send("please enter the FEN text. Leave empty for the initial chess game. Example: rnbqkb1r/pp3ppp/4pn2/3p4/2PP4/2N5/PP3PPP/R1BQKBNR w KQkq -");
+        builder.Prompts.text(session, "please enter the moves history. Leave empty for the initial chess game. Example: e2e4 e7e5 g1f3 b8c6 f1c4");
+    },
+    function (session, results) {
+        ///TODO: perform moves validation step here
+        ///TODO: use function
+        session.send("ok. let me check the next best move for you....");
+        engine
+            .nextMove(results.response)
+            .then((result) => {
+                session.send('I suggest you that one: ', result.nextMove);
+
+                ///TODO: call a dialog which prompts for help on visualizing on the board the next move and redirects to lichess or a are you curious to know what is your opponent next best move
+
+                // End
+                session.endDialog();
+            });
+    }
+
+]);
---
+++ engine/engine.js
@@ -0,0 +1,79 @@
+/**
+ * Created by danielr on 10/12/16.
+ */
+var Promise = require('bluebird');
+var stockfish = require("../lib/stockfish.js");
+
+var DEFAULT_ANALYZING_TIME = 5000;
+
+var engine = stockfish();
+
+module.exports = {
+    nextMove: function (situation) {
+        return new Promise(function (resolve) {
+
+            var position = "startpos";
+            var got_uci;
+            var started_thinking;
+
+            function send(str) {
+                console.log("Sending: " + str)
+                engine.postMessage(str);
+            }
+
+            engine.onmessage = function (line) {
+                var match;
+                console.log("Line: " + line)
+
+                if (typeof line !== "string") {
+                    console.log("Got line:");
+                    console.log(typeof line);
+                    console.log(line);
+                    return;
+                }
+
+                if (!got_uci && line === "uciok") {
+                    got_uci = true;
+                    if (position) {
+                        send("position " + position);
+                        //send("eval");
+                        send("d");
+                    }
+
+                    send("go ponder");
+                } else if (!started_thinking && line.indexOf("info depth") > -1) {
+                    console.log("Thinking...");
+                    started_thinking = true;
+                    send("go movetime " + DEFAULT_ANALYZING_TIME);
+
+                } else if (line.indexOf("bestmove") > -1) {
+                    match = line.match(/bestmove\s+(\S+)/);
+                    if (match) {
+                        var result = {};
+                        result.nextMove = match[1];
+                        console.log("Best move: " + result.nextMove);
+                        match = line.match(/ponder\s+(\S+)/);
+                        if (match) {
+                            result.nextOpponentMove = match[1];
+                            console.log("Next opponent move: " + result.nextOpponentMove);
+                        }
+                        resolve(result);
+                    }
+
+                }
+            };
+
+            (function get_position() {
+                if (situation.indexOf("/") > -1) {
+                    position = "fen " + situation;
+                } else {
+                    position = "startpos moves " + situation;
+                }
+            }());
+
+            send("uci");
+
+        });
+    }
+
+};
